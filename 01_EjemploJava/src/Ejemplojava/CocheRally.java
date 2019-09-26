dfaed61ed782b960335f88b9e3d26bdfbbeedfd8
David Domene Domene <daviddomene97@gmail.com>
2019-09-26 11:37:17 +0200

    WebServlet

--- 01_EjemploJava/src/Ejemplojava/CocheRally.java
+++
@@ -1,9 +0,0 @@
-package Ejemplojava;
-
-/**
- *
- * @author David Domene Domene
- */
-public class CocheRally extends Coche{
-    
-}
--- 01_EjemploJava/src/Ejemplojava/Main.java
+++ 01_EjemploJava/src/Ejemplojava/Main.java
@@ -3,45 +3,80 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Ejemplojava;
+package ejemplojava;
+
+import clasesjava.ProbandoString;
+import poo.Coche;
+import poo.CocheRally;
+import ejemplojava.NewClass;
+import poo.TipoCarburante;
 
 /**
  *
- * @author alumno
+ * @author Admin
  */
 public class Main {
 
+    public static void main(String[] args) {
+        // ProbandoString.tratarCadenas();
+        mainCoches(args);
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Empezamos POO");
-        Coche miCoche = new Coche("citroen", true, "roJO", TipoCarburante.GASOLINA);
-
-
-        miCoche.setArrancado(true);
-        miCoche.echarCarburante(50.0f);
-
-        System.out.println("Marca: " + miCoche.getMarca());
-
-        System.out.println("Capacidad deposito " + miCoche.CAPACIDAD_DEPOSITO);
-        System.out.println("¿Es Gasolina? " + miCoche.isEsGasolina());
-        System.out.println("Numero de ruedas: " + miCoche.getNumRuedas());
-        System.out.println("Nivel de Deposito: " + miCoche.getNivelDeposito());
-
-        //Coche otroCoche = new Coche();
-        //otroCoche.marca = "Ferrari";
-        //otroCoche.capacidadDeposito = 100;
-
-        miCoche.vaciarDeposito();
-
-        for (int i = 0; i < 3; i++) {
-            miCoche.acelerar();
-        }
+    public static void mainCoches(String[] args) {
         
+        NewClass nc = new NewClass();
+           
+        
+        System.out.println("Empezando POO");
+        Coche miCoche;
+        
+       // miCoche = new Coche("Kia", TipoCarcubrante.GASOLINA, TipoColor.BLANCO); // La instanciacion se hace con new
+        miCoche = new Coche("Kia", TipoCarburante.GASOLINA, "BlANCO"); // La instanciacion se hace con new
+        
+        miCoche.echarCarburante(30); // Empezamos con medio deposito
+        
+        System.out.println("Todos los coches tienen: " + Coche.CAPACIDAD_DEPOSITO);
+        System.out.println("Coche creado: " + miCoche.getMarca());
+        System.out.println("Nivel deposito: " + miCoche.getNivDeposito() + " litros");
+        
+        
+        Coche otroCoche = miCoche; // new Coche();
+        // otroCoche.marca = "Seat"; YA NO SE PUEDE
+        System.out.println("Coche creado: " + otroCoche.getMarca());
+        // vaciarDeposito(miCoche);
+        miCoche.vaciarDeposito();
+        System.out.println("Nivel deposito: " + otroCoche.getNivDeposito()+ " litros");
+        
+        miCoche.setArrancado(true);
+        miCoche.echarCarburante(-2);
+        for (int i = 0; i < 3; i++) 
+            miCoche.acelerar();
         
         miCoche.mostrar();
-
-    }
-
+        
+        CocheRally miSupra = new CocheRally("TOYOTA", TipoCarburante.GASOLINA, "NEGRO", 2.1f);
+        miSupra.echarCarburante(40);
+        miSupra.setArrancado(true);
+        miSupra.acelerar();
+        miSupra.acelerar();
+        miSupra.derrapar();
+        //miSupra.setNivDeposito(4); Solo dentro del paquete
+        //miSupra.explosionCilindro();
+        miSupra.mostrar();
+        
+        Coche supraNormal = miSupra;    // Conversión implícita
+        //supraNormal.explosionCilindro();
+        // supraNormal.derrapar();
+        supraNormal.acelerar();
+        supraNormal.mostrar();
+        
+        CocheRally cr = (CocheRally) supraNormal;   // Conversión explícita ó casting
+        System.out.println("Y el rozamiento?? " + cr.getRozamiento());
+        
+        // Esto lanza excepcion:        
+       /* cr = (CocheRally) miCoche;
+        System.out.println("Y el rozamiento?? " + cr.getRozamiento());*/
+    }    
 }
